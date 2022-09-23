ea52c0c1945439b5ebc4186cda17b4a1971e2bc8
sehong <wrsf8989@daum.net>
2022-09-23 22:32:57 +0900

    baekjoon

--- algorithm/src/baekjoon/BreakEvenPoint.java
+++ algorithm/src/baekjoon/BreakEvenPoint.java
@@ -7,7 +7,7 @@ public class BreakEvenPoint {
 	public static void main(String[] args) {
 		// 손익분기점
 		Scanner scan  = new Scanner(System.in); //스캐너
-		
+
 		int fixed = scan.nextInt(); //고정비용 입력		
 		int varIable = scan.nextInt(); //가변비용 입력 
 		int cost = scan.nextInt(); // 상품가격 입력
